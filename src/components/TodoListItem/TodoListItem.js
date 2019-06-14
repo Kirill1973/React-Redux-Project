7e277e799bdc803368ea70431b87260644535d8b
Kirill Pietkov <kirill99.pietkov@gmail.com>
2019-06-14 15:09:44 +0300

    add: check mark

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar, faEdit, faTimes } from '@fortawesome/free-solid-svg-icons';
+import {
+  faStar, faEdit, faTimes,
+} from '@fortawesome/free-solid-svg-icons';
 import Style from './TodoListItem.module.scss';
 
 
